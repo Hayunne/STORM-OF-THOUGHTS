c06723e4ae8699e10d60ca0e663fa5d1dc506f36
aluno <aluno@aluno.com>
2019-03-25 16:43:28 -0300

    Inicio do formulario de login

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-layout>
+        <v-flex xs3>
+            <v-card>
+                <v-text-field label="teste"></v-text-field>
+                <v-btn color="Blue">Teste</v-btn>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
