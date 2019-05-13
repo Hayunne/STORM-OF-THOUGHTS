db53314e7289ab4cd0c3ae173464552334f3ce71
aluno <aluno@aluno.com>
2019-05-13 17:27:18 -0300

    Término do estilo do login e começo do cadastro (estilo)

---
+++ src/views/Cadastro.vue
@@ -0,0 +1,74 @@
+<template>
+
+<v-layout justify-center align-center fill-height class="cadastro">
+      <v-flex xs12 sm6 md4 lg4 >
+      <v-card color="#F79F81" class="elevation-12 pa-5">
+ 
+ 
+  
+        <form>    
+              <v-text-field
+                v-model="nome"
+                label="Nome Completo"
+                required
+                ></v-text-field>
+
+                <v-text-field
+                v-model="nome"
+                label="Nome de usuário"
+                required
+                ></v-text-field>
+
+                 <v-text-field
+                v-model="email"
+                label="Idade"
+                 ></v-text-field>
+               
+
+              <v-text-field
+                v-model="email"
+                :error-messages="emailErrors"
+                label="E-mail"
+                required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="email"
+                :error-messages="senhaErrors"
+                label="Senha"
+                required
+                @input="$v.senha.$touch()"
+                @blur="$v.senha.$touch()"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="email"
+                :error-messages="senhaErrors"
+                label="Confirmar Senha"
+                required
+                @input="$v.senha.$touch()"
+                @blur="$v.senha.$touch()"
+              ></v-text-field>
+
+                
+              <v-btn block @click="submit">Entrar</v-btn>
+             
+          </form>
+            </v-card>
+        </v-flex>
+        
+    </v-layout>
+</template>
+
+ <script> 
+   </script>
+      <style> 
+      .cadastro {
+        height: 100%;
+        background-image: url("https://www.wallpapermaiden.com/wallpaper/9881/download/1366x768/firewatch-forest-artwork.jpg");
+      }
+      
+
+      </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,9 @@
 
 <template>
-  <div>
-     <v-layout>
-        <v-flex xs12 sm6 offset-sm3>
+  
+     <v-layout justify-center align-center fill-height class="login">
+     
+        <v-flex xs12 sm6 md4 lg4 >
            <v-card color="#F79F81" class="elevation-12 pa-5">
           <form>
           
@@ -22,12 +23,24 @@
                 @input="$v.senha.$touch()"
                 @blur="$v.senha.$touch()"
               ></v-text-field>
-              <v-layout align-center justify-center>
-              <v-btn @click="submit">Entrar</v-btn>
-              </v-layout>
+               
+              <v-btn block @click="submit">Entrar</v-btn>
+             
           </form>
         </v-card>
         </v-flex>
+        
     </v-layout>
-  </div>
+
 </template>
+  <script> 
+   </script>
+      <style> 
+      .login {
+        height: 100%;
+        background-image: url("https://c4.wallpaperflare.com/wallpaper/8/169/216/firewatch-digital-art-birds-forest-wallpaper-7960c8edb12add9b76a7786fa001a6dd.jpg");
+      }
+      
+      
+      
+      </style>
